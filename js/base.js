b06334e10b3497198970ff442771df77d98463d9
hdg <hdg1988@gmail.com>
2014-03-24 02:32:24 +0800

    first commint

---
+++ js/base.js
@@ -0,0 +1,166 @@
+!!(function(){
+	// game mgr 
+	var gameMgr = (function(){
+    	var gameMgr = {
+    		_html : $('.game-inner').html(),
+    		start: function(){
+    			$('.game-inner').find('[data-animate]')
+		            .each(function(){
+		                var $dom = $(this);
+		                var tar = $dom.data('animate');
+		                var time = parseInt( $dom.data('time') );
+		                var delay = $dom.data('delay') || 0;
+		                var easing = $dom.data('easing');
+		                var begin = $dom.data('begin');
+		                tar = tar.split(';');
+		                var tarCss = {} , tmp;
+		                for (var i = tar.length - 1; i >= 0; i--) {
+		                    tmp = tar[i].split(':');
+		                    if( tmp.length == 2 )
+		                        tarCss[ tmp[0] ] = $.trim(tmp[1]);
+		                }
+		                $dom.delay( delay )
+		                    .animate( tarCss , time , easing );
+		                
+		            });
+    		},
+    		reset: function(){
+    			$('.game-inner').html( gameMgr._html )
+    				.fadeIn();
+    			$('.ques-inner').fadeOut();
+    			throwBall = false;
+    			gameMgr.start();
+    		},
+    		stopFlicker: function(){
+    			clearTimeout( gameMgr._timer );
+    		},
+    		flicker: function( $dom ){
+    			var times = 0;
+    			!function caller(){
+    				if( ++times > 7 ) return;
+    				$dom[$dom.is(':visible') ? 'fadeOut' : 'fadeIn']( 250 );
+    				gameMgr._timer = setTimeout(function(){
+    					caller( $dom );
+    				} , 280)
+    			}();
+    		},
+    		throwBall: function( $tar , cb ){
+    			var top = parseInt( $tar.css('top') );
+        		var left = parseInt( $tar.css('left') );
+        		var center = {top: top + 108 / 2 , left: left + 108/2 };
+
+        		// run animate
+        		var $ball = $('.game-ball');
+        		var tar = {
+        			width: 50,
+        			height: 50,
+        			zIndex: 1,
+        			left: center.left - 25,
+        			top: center.top - 25,
+        			rotate: 140 + ~~( 240 * Math.random() )
+        		}
+
+
+        		var curr = {
+        			width: $ball.width(),
+        			height: $ball.height(),
+        			zIndex: 100,
+        			left: parseInt( $ball.css('left') ),
+        			top:  parseInt( $ball.css('top') ),
+        			rotate: 0
+        		}
+
+        		var time = 1000;
+        		var vy = -1;
+        		var a = ( ( tar.top - curr.top ) - vy * time ) * 2 / time / time ;
+
+        		var startTime = new Date();
+        		setTimeout(function animate(){
+        			var d = new Date() - startTime;
+        			if( d > time ){
+        				$ball.css(tar);
+        				cb && cb();
+        				return;
+        			}
+
+        			var p = d / time;
+        			var status = {
+        				width: curr.width + ( - curr.width + tar.width ) * p,
+        				height: curr.height + ( - curr.height + tar.height ) * p,
+        				zIndex: curr.zIndex + ( - curr.zIndex + tar.zIndex ) * p,
+        				left: curr.left + ( tar.left - curr.left ) * p,
+        				top: curr.top + vy * d + 1 / 2 * a * d * d 
+        			}
+
+        			$ball.css( status );
+        			var r = "rotate(" + parseInt( curr.rotate + ( tar.rotate - curr.rotate ) * p ) + 'deg)';
+        			$ball[0].style['-webkit-transform'] = r;
+        			$ball[0].style['-ms-transform'] = r;
+        			$ball[0].style['-moz-transform'] = r;
+        			$ball[0].style['-o-transform'] = r;
+        			setTimeout( animate , 1000 / 60 );
+        		} , 1000/60);
+    		}
+    	}
+        
+
+        var $wrap = $('.game-wrap');
+        $wrap.on( 'click'  , '.game-btn' , function(){
+        		$wrap.find('.game-g1')
+        			.animate({left: 700} , 500)
+
+        			.end()
+        			.find('.game-btn')
+        			.delay(300)
+        			.animate({top: -90} , 500)
+
+        			.end()
+        			.find('.game-w')
+        			.animate({bottom:-68} , 500)
+
+        			.end()
+        			.find('.game-g2')
+        			.show()
+        			.delay(200)
+        			.animate({left:20} , 500)
+
+        			.end()
+        			.find('.game-g3')
+        			.delay(800)
+        			.animate({left:577} , 500)
+
+        			.end()
+        			.find('.game-g4')
+        			.delay( 400 )
+        			.animate({left:615} , 500);
+
+
+        			//Flicker dir
+        		gameMgr.flicker($wrap.find('.game-dir'));
+
+        		$wrap.find('.game-tar1,.game-tar2,.game-tar3,.game-tip')
+        			.fadeIn( 1000 );
+        	});
+
+        var throwBall = false;
+        $wrap.on( 'click' , '.game-tar1,.game-tar2,.game-tar3' , function(){
+        		if( throwBall ) return;
+        		throwBall = true;
+
+        		gameMgr.stopFlicker();
+        		$('.game-dir').stop( true , true )
+        			.fadeOut();
+        		gameMgr.throwBall( $(this) , function(){
+        			//throwBall = false;
+
+        			$('.game-inner').fadeOut();
+        			$('.ques-inner').fadeIn();
+        		} );
+        	});
+
+        return gameMgr;
+    })();
+
+
+    gameMgr.start();
+})();
