563d4d99ab9f51c7b9fcda5fa998485b70c51bf1
hdg <hdg1988@gmail.com>
2014-04-01 01:22:49 +0800

    fix

--- js/base.js
+++ js/base.js
@@ -4,6 +4,7 @@
     	var gameMgr = {
     		_html : $('.game-inner').html(),
     		start: function(){
+                isGameStart = false;
     			$('.game-inner').find('[data-animate]')
 		            .each(function(){
 		                var $dom = $(this);
@@ -44,72 +45,189 @@
     				} , 280)
     			}();
     		},
-    		throwBall: function( $tar , cb ){
-    			var top = parseInt( $tar.css('top') );
-        		var left = parseInt( $tar.css('left') );
-        		var center = {top: top + 108 / 2 , left: left + 108/2 };
+            isMeet: function( pos1 , pos2 , r ){
+                var dis = gameMgr.getDistance( pos1 , pos2 );
+            },
+            throwBall: function( info , success , failure ){
+                // var top = parseInt( $tar.css('top') );
+                // var left = parseInt( $tar.css('left') );
+                // var center = { top: top + 108 / 2 , left: left + 108 / 2 };
 
-        		// run animate
-        		var $ball = $('.game-ball');
-        		var tar = {
-        			width: 50,
-        			height: 50,
-        			zIndex: 1,
-        			left: center.left - 25,
-        			top: center.top - 25,
-        			rotate: 140 + ~~( 240 * Math.random() )
-        		}
+                // // run animate
+                var $ball = $('.game-ball');
+                // var tar = {
+                //     width: 50,
+                //     height: 50,
+                //     zIndex: 1,
+                //     left: center.left - 25,
+                //     top: center.top - 25,
+                //     rotate: 140 + ~~( 240 * Math.random() )
+                // }
 
 
-        		var curr = {
-        			width: $ball.width(),
-        			height: $ball.height(),
-        			zIndex: 100,
-        			left: parseInt( $ball.css('left') ),
-        			top:  parseInt( $ball.css('top') ),
-        			rotate: 0
-        		}
+                var curr = {
+                    width: $ball.width(),
+                    height: $ball.height(),
+                    zIndex: 1000,
+                    left: parseInt( $ball.css('left') ),
+                    top:  parseInt( $ball.css('top') ),
+                    rotate: 0
+                }
+                curr.cx = curr.left + curr.width / 2;
+                curr.cy = curr.top + curr.height / 2;
+                //var time = 1000;
+                var vy = info.vy;
+                var vx = info.vx;
+                var vz = info.vz;//2.5;
+                var zDis = 500;
+                var a = 0.00202 ;
+                // var a = ( ( tar.top - curr.top ) - vy * time ) * 2 / time / time ;
 
-        		var time = 1000;
-        		var vy = -1.2;
-        		var a = ( ( tar.top - curr.top ) - vy * time ) * 2 / time / time ;
+                var startTime = new Date();
+                var $stars = $('.game-tar1,.game-tar2,.game-tar3');
+                var centers = [];
+                $stars.each( function(){
+                      centers.push( $(this).offset() );
+                });
+                setTimeout(function animate(){
+                    var d = new Date() - startTime;
+                    
+                    //var p = d / time;
+                    var w = curr.width - ( vz * d / zDis * 150 );
+                    var status = {
+                        width: w,
+                        height: w,
+                        zIndex: curr.zIndex - ( vz * d / zDis * 150 ) ,
+                        left: curr.cx + vx * d - w / 2 ,
+                        top: curr.cy + vy * d - w / 2 + 1 / 2 * a * d * d 
+                    }
+                    if( vz * d >= zDis - 50 && vz * d <= zDis + 50 ){
+                        //判断是否相交 
+                        var isMeet = false;
+                        $.each( centers , function( i , pos ){
+                            var dis = gameMgr.getDistance( pos , {left: status.left + status.width / 2 , top: status.top + status.width / 2} );
+                            if( dis < 80 ){
+                                isMeet = true;
+                                return false;
+                            }
+                        });
+                        if( isMeet ){
+                            success && success();
+                            return;
+                        }
+                    }
+                    if( status.zIndex <= 0 || status.left < 0 || status.left > 1000 || status.top > 1000 ){
+                        $('.game-ball')
+                            .animate( {
+                                top: 264
+                            } , 500 )
+                            [0].style.cssText = 'top: 370px;';
+                        failure && failure();
+                        return ;
+                    }
+                    console.log( status.zIndex )
 
-        		var startTime = new Date();
-        		setTimeout(function animate(){
-        			var d = new Date() - startTime;
-        			if( d > time ){
-        				$ball.css(tar);
-        				cb && cb();
-        				return;
-        			}
+                    // // fix overflow
+                    // if( d / time > 0.3 ){
+                    //     $wrap.css('overflow' , 'visible');
+                    // }
 
-        			var p = d / time;
-        			var status = {
-        				width: curr.width + ( - curr.width + tar.width ) * p,
-        				height: curr.height + ( - curr.height + tar.height ) * p,
-        				zIndex: curr.zIndex + ( - curr.zIndex + tar.zIndex ) * p,
-        				left: curr.left + ( tar.left - curr.left ) * p,
-        				top: curr.top + vy * d + 1 / 2 * a * d * d 
-        			}
+                    $ball.css( status );
+                    var r = "rotate(" + parseInt( curr.rotate + d / 1000 * 360 ) + 'deg)';
+                    $ball[0].style['-webkit-transform'] = r;
+                    $ball[0].style['-ms-transform'] = r;
+                    $ball[0].style['-moz-transform'] = r;
+                    $ball[0].style['-o-transform'] = r;
+                    setTimeout( animate , 1000 / 60 );
+                } , 1000 / 60);
+            },
+    		// throwBall: function( $tar , cb ){
+    		// 	var top = parseInt( $tar.css('top') );
+      //   		var left = parseInt( $tar.css('left') );
+      //   		var center = {top: top + 108 / 2 , left: left + 108/2 };
 
-        			// fix overflow
-        			if( d / time > 0.3 ){
-        				$wrap.css('overflow' , 'visible');
-        			}
+      //   		// run animate
+      //   		var $ball = $('.game-ball');
+      //   		var tar = {
+      //   			width: 50,
+      //   			height: 50,
+      //   			zIndex: 1,
+      //   			left: center.left - 25,
+      //   			top: center.top - 25,
+      //   			rotate: 140 + ~~( 240 * Math.random() )
+      //   		}
 
-        			$ball.css( status );
-        			var r = "rotate(" + parseInt( curr.rotate + ( tar.rotate - curr.rotate ) * p ) + 'deg)';
-        			$ball[0].style['-webkit-transform'] = r;
-        			$ball[0].style['-ms-transform'] = r;
-        			$ball[0].style['-moz-transform'] = r;
-        			$ball[0].style['-o-transform'] = r;
-        			setTimeout( animate , 1000 / 60 );
-        		} , 1000/60);
-    		}
+
+      //   		var curr = {
+      //   			width: $ball.width(),
+      //   			height: $ball.height(),
+      //   			zIndex: 100,
+      //   			left: parseInt( $ball.css('left') ),
+      //   			top:  parseInt( $ball.css('top') ),
+      //   			rotate: 0
+      //   		}
+
+      //   		var time = 1000;
+      //   		var vy = -1.2;
+      //   		var a = ( ( tar.top - curr.top ) - vy * time ) * 2 / time / time ;
+      //           console.log( a );
+      //   		var startTime = new Date();
+      //   		setTimeout(function animate(){
+      //   			var d = new Date() - startTime;
+      //   			if( d > time ){
+      //   				$ball.css(tar);
+      //   				cb && cb();
+      //   				return;
+      //   			}
+
+      //   			var p = d / time;
+      //   			var status = {
+      //   				width: curr.width + ( - curr.width + tar.width ) * p,
+      //   				height: curr.height + ( - curr.height + tar.height ) * p,
+      //   				zIndex: curr.zIndex + ( - curr.zIndex + tar.zIndex ) * p,
+      //   				left: curr.left + ( tar.left - curr.left ) * p,
+      //   				top: curr.top + vy * d + 1 / 2 * a * d * d 
+      //   			}
+
+      //   			// fix overflow
+      //   			if( d / time > 0.3 ){
+      //   				$wrap.css('overflow' , 'visible');
+      //   			}
+
+      //   			$ball.css( status );
+      //   			var r = "rotate(" + parseInt( curr.rotate + ( tar.rotate - curr.rotate ) * p ) + 'deg)';
+      //   			$ball[0].style['-webkit-transform'] = r;
+      //   			$ball[0].style['-ms-transform'] = r;
+      //   			$ball[0].style['-moz-transform'] = r;
+      //   			$ball[0].style['-o-transform'] = r;
+      //   			setTimeout( animate , 1000 / 60 );
+      //   		} , 1000/60);
+    		// },
+
+            getDistance: function( pos1 , pos2 ){
+                return Math.sqrt( Math.pow( pos1.left - pos2.left , 2 ) + Math.pow( pos1.top - pos2.top , 2 ) );
+            },
+
+            prepareThrow: function( strPos , tarPos ){
+                var dis = gameMgr.getDistance( strPos , tarPos );
+                // 距离与速度的关系
+                var speed = 1.2 * dis / 300;
+                var angle = Math.atan( ( tarPos.top - strPos.top ) / ( tarPos.left - strPos.left ) );
+                //var vy = 
+                var vx = ( tarPos.left > strPos.left ? 1 : -1 ) * speed * Math.abs( Math.cos( angle ) ) / 3.5;
+                var vy = - speed * Math.abs( Math.sin( angle ) );
+                return {
+                    vx : vx ,
+                    vy : vy ,
+                    vz : 0.5 ,//+ Math.abs(  3 * vx ),
+                    dis: dis
+                }
+            }
     	}
         
 
         var $wrap = $('.game-wrap');
+        var isGameStart = false;
         $wrap.on( 'click'  , '.game-btn' , function(){
         		$wrap.find('.game-g1')
         			.animate({left: 700} , 500  , function(){$(this).hide()})
@@ -145,45 +263,51 @@
 
         		$wrap.find('.game-tar1,.game-tar2,.game-tar3,.game-tip')
         			.fadeIn( 1000 );
+                isGameStart = true;
+
         	});
 
         var isMouseDown = false;
         var startPos = null;
-
-        $(document)
+        var throwBall = false;
+        var minDis  = 100;
+        $wrap
             // drag to throw ball
             .on('mousedown' , '.game-ball' , function( ev ){
-                isMouseDown = true;
-                startPos = { left: ev.pageX , top: ev.pageY };
+                if( isGameStart ){
+                    isMouseDown = true;
+                }
             })
-            .on('mousemove' , function(){
+            .on('mousemove.ball-drag' , function( ev ){
                 if( !isMouseDown ) return;
                 //TODO：： 可以显示 一个蓄力的
             })
-            .on('mouseup' , function(){
-                if( isMouseDown ){
-                    //  计算位置和方向
-                    
+            .on('mouseup' , function( ev ){
+                if( isMouseDown && !throwBall ){
+                    throwBall = true;
+
+                    gameMgr.stopFlicker();
+                    $('.game-dir').stop( true , true )
+                        .fadeOut();
+
+                    var off = $('.game-ball').offset();
+                    var w = $('.game-ball').width();
+                    startPos = { left: off.left + w / 2 , top: off.top + w / 2 };
+                    gameMgr.throwBall( gameMgr.prepareThrow( startPos , {left: ev.pageX , top: ev.pageY} ) , function(){
+                        throwBall = false;
+                        $('.game-inner').fadeOut();
+                        $('.ques-inner').fadeIn();
+                    } , function(){
+                        throwBall = false;
+                        $('.game-dir').fadeIn();
+                    } );
                 }
+            });
+        $(document)
+            .on('mouseup' , function( ev ){
                 isMouseDown = false;
             });
-
-        var throwBall = false;
-        $wrap.on( 'click' , '.game-tar1,.game-tar2,.game-tar3' , function(){
-        		if( throwBall ) return;
-        		throwBall = true;
-
-        		gameMgr.stopFlicker();
-        		$('.game-dir').stop( true , true )
-        			.fadeOut();
-        		gameMgr.throwBall( $(this) , function(){
-        			//throwBall = false;
-
-        			$('.game-inner').fadeOut();
-        			$('.ques-inner').fadeIn();
-        		} );
-        	});
-
+        
         return gameMgr;
     })();
 
